f8f4c2d7e53ae55d1a7fb71bf0b90550db842be8
Diego Barrionuevo <santosdiegob@gmail.com>
2014-07-31 18:01:55 -0300

    Boceto del diseño.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
 require_once 'config.php';
-echo '¡Estoy vivo, hola mundo!';
+
 require_once 'tmpl/index.tmpl.php';
--- tmpl/index.tmpl.php
+++ tmpl/index.tmpl.php
@@ -3,10 +3,35 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <!-- Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Roboto|Nunito:700' rel='stylesheet' type='text/css' />
         <!-- CSS Index -->
+        <link href="<?php echo $global_ruta_web; ?>/css/general.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo $global_ruta_web; ?>/css/botones.css" rel="stylesheet" type="text/css" />
         <!-- JavaScript Index -->
+        <script src="<?php echo $global_ruta_web; ?>/js/lib/jquery-1.11.1.min.js" type="text/javascript"></script>
     </head>
-    <body>
-        
+    <body class="fontInformal">
+        <div id="cabecera">
+            <h1 id="tituloSitio" class="fontFormal">
+                Generador de Encuestas - UTN
+            </h1>
+        </div>
+        <div id="contenido">
+            <div class="pregunta">
+                <div class="seccionPregunta">
+                    <h3 class="tituloPregunta">
+                        ¿Qué tipo de preguntas puedo hacer?
+                    </h3>
+                </div>
+                <div class="seccionRespuesta">
+                    <input type="text" value="Respuesta" class="inputTexto"/>
+                </div>
+            </div>
+            <div class="piePregunta">
+                <form action="#" method="post">
+                    <input type="submit" value="Enviar" class="boton botonWindows azul"/>
+                </form>
+            </div>
+        </div>
     </body>
 </html>
